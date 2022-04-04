8fa7c14ab6a9d57fd1920d4622a434783dc3efbd
Daniel Castro <danielcastro.rs@gmail.com>
2022-04-04 12:55:39 -0300

    feat: add new tests for login route

--- tests/integration/customers.test.ts
+++ tests/integration/customers.test.ts
@@ -1,6 +1,7 @@
 import request, { SuperTest, Test } from "supertest";
 import httpStatus from "http-status";
 import { app } from "@/app";
+import { authenticationService } from "@/services";
 
 let server: SuperTest<Test> = null;
 
@@ -15,6 +16,29 @@ describe("Server tests", () => {
       const res = await server.get("/customers").send();
 
       expect(res.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+
+    it(`should respond with status ${httpStatus.UNAUTHORIZED} with token auth invalid`, async () => {
+      const res = await server
+        .get("/customers")
+        .set("Authorization", "Bearer 12345")
+        .send();
+
+      expect(res.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+
+    it(`should respond with status ${httpStatus.OK} with token auth valid`, async () => {
+      const payload = {
+        id: 1,
+      };
+      const tokenValid = authenticationService.tokenGenerate(payload);
+
+      const res = await server
+        .get("/customers")
+        .set("Authorization", `Bearer ${tokenValid}`)
+        .send();
+
+      expect(res.status).toBe(httpStatus.OK);
     });
   });
 });
