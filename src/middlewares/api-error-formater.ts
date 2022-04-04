cebdc11caa84ec5e838c6bc7a6bebe84343ffd91
Daniel Castro <danielcastro.rs@gmail.com>
2022-04-03 22:01:23 -0300

    fix: fixed treatment custom errors

--- src/middlewares/api-error-formater.ts
+++ src/middlewares/api-error-formater.ts
@@ -9,14 +9,14 @@ export const apiFormatError = (
   next: NextFunction
 ) => {
   let status = httpStatus.INTERNAL_SERVER_ERROR;
+  let shouldLog = false;
 
   if (error instanceof NotFoundError) status = httpStatus.NOT_FOUND;
-  if (error instanceof UnauthorizedError) status = httpStatus.UNAUTHORIZED;
-  if (error instanceof BadRequestError) status = httpStatus.UNAUTHORIZED;
-  else {
-    console.log(error);
-    error = new Error("Internal Server Error");
-  }
+  else if (error instanceof UnauthorizedError) status = httpStatus.UNAUTHORIZED;
+  else if (error instanceof BadRequestError) status = httpStatus.BAD_REQUEST;
+  else shouldLog = true;
+
+  shouldLog && console.log(error);
 
   res.status(status).json({ name: error.name, message: error.message });
 };
